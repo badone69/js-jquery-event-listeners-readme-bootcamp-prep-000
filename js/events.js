d3413251e1566e1738e26dc4c543422ab8aa6b6f
student online <eliel.badon@gmail.com>
2018-08-14 22:29:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -25,12 +25,11 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form').on('submit', function(){
-    alert('your form is going to be submitted now');
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
     return;
-  })
-  
-}
+  }
+);}
 
 $(document).ready(function(){
 

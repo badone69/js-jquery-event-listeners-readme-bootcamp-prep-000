c69c6bfec0a32fc746f220ffbe608cc6092a5bef
student online <eliel.badon@gmail.com>
2018-08-14 22:29:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -25,8 +25,8 @@ function pressIt() {
 }
 
 function submitIt() {
-  $("form").on("submit", function(){
-    alert('Your form is going to be submitted now');
+  $('form').on('submit', function(){
+    alert('your form is going to be submitted now');
     return;
   })
   
